a5759dcf6ef77931e1a224468f1d1cd0661243c1
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-04-11 10:29:31 +0530

    fin .

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,9 +11,17 @@ const Homepage = () => {
       <div className="homepageWelcome">
         <h1>Welcome to iDreamCareer</h1>
         <p>We help to you chase your dreams.</p>
-
-        <br />
-        <br />
+        <p className="description">
+          iDreamCareer is the INDIA’S LARGEST unbiased career counseling venture that works with 2.5
+          MILLION STUDENTS yearly while ensuring 100% DATA PRIVACY. We currently work with 12 Indian
+          State Governments & 73000+ schools across India & the Middle East.
+        </p>
+        <p className="description">
+          We offer customized career counselling and guidance services to students based on their
+          specific needs. Ranging from technology enabled information access only, to human-led
+          professional career counselling, our offerings include college application and scholarship
+          guidance as well
+        </p>
       </div>
     </div>
   );
